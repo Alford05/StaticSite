cd8d3796fb57f43ae678ad6e9c27d8a87bdf8e02
Oakley <dalford278@gmail.com>
2025-03-30 12:55:16 -0400

    HTMLNode complete

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -5,12 +5,10 @@ class HTMLNode:
         self.children = children
         self.props = props
 
-
-    
     def __to_html__(self):
         raise NotImplementedError
     
-    def __props_to_html__(self);
+    def props_to_html(self):
         if not self.props:
             return ""
         props_str = ""
@@ -25,6 +23,43 @@ class HTMLNode:
         children: {self.children}, 
         props: {self.props}
         )"""
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, [], props)
+        self.value = value
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+        if self.tag is None:
+            return self.value
+        html = f"<{self.tag}"
+        if self.props:
+            for prop_name, prop_value in self.props.items():
+                html += f' {prop_name}="{prop_value}"'
+        html += f">{self.value}</{self.tag}>"
+        return html
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, [], children, props)
+        if not isinstance(children, list) or children == []:
+            raise ValueError("ParentNode must have a non-empty list of children")
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if self.children is None:
+            raise ValueError("ParentNode must have a child")
+        html = f"<{self.tag}>"
+        for child in self.children:
+            html += f"{child.to_html()}"
+        html += f"</{self.tag}>"
+        return html
+
+
 
 
 
