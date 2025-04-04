9a36d7a51db6edaa88574e0d26a7e2f487e4fdba
Oakley <dalford278@gmail.com>
2025-04-04 18:10:12 -0400

    Public to Main function

--- src/main.py
+++ src/main.py
@@ -1,28 +1,32 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
+import os
+import shutil
 
 
 def main():
     node = TextNode("This is some text", TextType.LINK, "https://example.com")
     print(node)
 
-def text_node_to_html_node(text_node):
-    if text_node.text_type == TextType.TEXT:
-        return LeafNode(None, text_node.text)
-    if text_node.text_type == TextType.BOLD:
-        return LeafNode("b", text_node.text)
-    if text_node.text_type == TextType.ITALIC:
-        return LeafNode("i", text_node.text)
-    if text_node.text_type == TextType.CODE:
-        return LeafNode("code", text_node.text)
-    if text_node.text_type == TextType.LINK:
-        return LeafNode("a", text_node.text, {"href": text_node.url})
-    if text_node.text_type == TextType.IMAGE:
-        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
-    raise ValueError(f"invalid text type: {text_node.text_type} does not compute")
-    
 
-    
+def copy_static(source_dir, dest_dir):
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+    os.mkdir(dest_dir)
+    items = os.listdir(source_dir)
+    for item in items:
+        source_item_path = os.path.join(source_dir, item)
+        dest_item_path = os.path.join(dest_dir, item)
+        if os.path.isfile(source_item_path):
+            shutil.copy(source_item_path, dest_item_path)
+            print(f"copied file: {source_item_path} to {dest_item_path}")
+        else:
+            os.mkdir(dest_item_path)
+            print(f"created Directory: {dest_item_path}")
+            copy_static(source_item_path, dest_item_path)
+            
+
+
     
 
 
